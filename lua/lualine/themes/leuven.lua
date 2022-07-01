831b1cbcd26a7e6dabc27cadd0478bfa999a0818
gzagatti <gzagatti@users.noreply.github.com>
2022-07-02 02:41:48 +0800

    starts with 8bit color.

--- lua/lualine/themes/leuven.lua
+++ lua/lualine/themes/leuven.lua
@@ -1,15 +1,18 @@
-return {
-  normal = {
-    a = {fg = "#f8fbf8", bg = "#335ea8", gui = "bold"},
-    b = {fg = "#85ceeb", bg = "#335ea8"},
-    c = {fg = "#85ceeb", bg = "#454545"},
-    y = {fg = "#85ceeb", bg = "#335ea8"},
-    z = {fg = "#85ceeb", bg = "#335ea8"},
+if vim.opt:get('termguicolors') then
+else
+  return {
+    normal = {
+      a = {fg = "#eeeeee", bg = "#005faf", gui = "bold"},
+      b = {fg = "#85ceeb", bg = "#005faf"},
+      c = {fg = "#85ceeb", bg = "#454545"},
+      y = {fg = "#85ceeb", bg = "#005faf"},
+      z = {fg = "#85ceeb", bg = "#005faf"},
 
-  },
-  inactive = {
-    a = {fg = "#f8fbf8", bg = "#a9a9a9"},
-    b = {fg = "#f8fbf8", bg = "#686868"},
-    c = {fg = "#f8fbf8", bg = "#a9a9a9"},
+    },
+    inactive = {
+      a = {fg = "#eeeeee", bg = "#a9a9a9"},
+      b = {fg = "#eeeeee", bg = "#686868"},
+      c = {fg = "#eeeeee", bg = "#a9a9a9"},
+    }
   }
-}
+end
